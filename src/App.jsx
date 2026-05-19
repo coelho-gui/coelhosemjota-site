934db8c45840d6292f67fc8ca4fb72a67ddb25e1
coelho-gui <71169119+coelho-gui@users.noreply.github.com>
2026-05-19 20:47:26 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Brasilidades from "./pages/Brasilidades";
+import Kobi from "./pages/Kobi";
 import "./styles.css";
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projetos/brasilidades" element={<Brasilidades />} />
+        <Route path="/projetos/kobi" element={<Kobi />} />
       </Routes>
     </BrowserRouter>
   );
