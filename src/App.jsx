d41c9de0984b19b6d5170cf086773dbdcfa00268
coelho-gui <71169119+coelho-gui@users.noreply.github.com>
2026-05-21 16:01:10 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Brasilidades from "./pages/Brasilidades";
 import Kobi from "./pages/Kobi";
 import Argila from "./pages/Argila";
+import PizzaFun from "./pages/PizzaFun";
 import LadoB from "./pages/LadoB";
 import "./styles.css";
 
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/projetos/brasilidades" element={<Brasilidades />} />
         <Route path="/projetos/kobi" element={<Kobi />} />
         <Route path="/projetos/argila" element={<Argila />} />
+        <Route path="/projetos/pizza-fun" element={<PizzaFun />} />
         <Route path="/lado-b" element={<LadoB />} />
       </Routes>
     </BrowserRouter>
